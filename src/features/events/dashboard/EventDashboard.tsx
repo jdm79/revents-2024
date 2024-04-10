3e777278ebc334596c063634f69c29f3c8736e0d
jdm79 <james.malvern@gmail.com>
2024-04-10 20:31:59 +0800

    add use effect to load the sample data when the page is first opnene

--- src/features/events/dashboard/EventDashboard.tsx
+++ src/features/events/dashboard/EventDashboard.tsx
@@ -1,9 +1,10 @@
 import { Grid } from "semantic-ui-react";
 import EventList from "./EventList";
 import EventForm from "../form/EventForm";
-import { sampleData } from "../../../app/api/sampleData";
-import { useState } from "react";
+// import { sampleData } from "../../../app/api/sampleData";
+import { useEffect, useState } from "react";
 import { AppEvent } from "../../../app/types/event";
+import { sampleData } from "../../../app/api/sampleData";
 
 type Props = {
   formOpen: boolean;
@@ -12,11 +13,16 @@ type Props = {
 
 export default function EventDashboard({ formOpen, setFormOpen }: Props) {
   const [events, setEvents] = useState<AppEvent[]>([]);
+  // using an empty dependencies array makes sure this is run only once
+  // this useEffect hook is like componentDidMount etc from the old days
+  useEffect(() => {
+    setEvents(sampleData);
+  }, []);
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={sampleData} />
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
         {formOpen && <EventForm setFormOpen={setFormOpen} />}
